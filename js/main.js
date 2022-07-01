aaf7220a85250505d63a4bd26a4b41ee0563f430
Nur Akhtar <58050598+mnakhtar555@users.noreply.github.com>
2022-07-01 17:58:27 +0600

    init

--- js/main.js
+++ js/main.js
@@ -25,3 +25,26 @@ for( let i = 0; i < navbarLinks.length; i++ ){
         overlay.classList.toggle('active');
     });
 }
+
+// Header & Go Top Btn active when window scroll down to 400px
+const header = document.querySelector('[data-header]');
+const goTopBtn = document.querySelector('[data-go-top]');
+
+window.addEventListener( 'scroll', function(){
+    if( window.scrollY >= 400 ) {
+        header.classList.add('active');
+        goTopBtn.classList.add('active');
+    }else{
+        header.classList.remove('active');
+        goTopBtn.classList.remove('active');
+    }
+});
+
+
+window.addEventListener( 'scroll', function(){
+    if( this.window.innerWidth >=992 && this.window.scrollY >=400 ) {
+        header.classList.add('header-desktop');
+    }else{
+        header.classList.remove('header-desktop' );
+    }
+});
